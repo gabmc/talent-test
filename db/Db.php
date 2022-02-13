f75d063789612b783fbc7899a634923996f8a17b
Gary Bustillos <garybustillos@gmail.com>
2022-02-13 11:27:34 +0100

    removed unused code

--- db/Db.php
+++ db/Db.php
@@ -21,14 +21,6 @@ class jsonDb implements Db {
     public static function select($table, $filter) {
         $savedData = file_get_contents(__DIR__."/data/{$table}.json");
         $savedData = json_decode($savedData);
-        // var_dump($savedData[1]->username);
-        // foreach($savedData as $key => $val) {
-        //     foreach($val as $key2 => $val2) {
-        //         if ($filter[$key2] === $val2) {
-        //             return $savedData[$key];
-        //         }
-        //     }
-        // }
         $count = 0;
         $output = array();
         foreach ($filter as $key => $val) {

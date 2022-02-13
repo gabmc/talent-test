c9c706d3701a2fd4e399488be48ed240374af3de
Gary Bustillos <garybustillos@gmail.com>
2022-02-13 11:13:44 +0100

    fixed select

--- db/Db.php
+++ db/Db.php
@@ -22,29 +22,30 @@ class jsonDb implements Db {
         $savedData = file_get_contents(__DIR__."/data/{$table}.json");
         $savedData = json_decode($savedData);
         // var_dump($savedData[1]->username);
-        foreach($savedData as $key => $val) {
-            foreach($val as $key2 => $val2) {
-                if ($filter[$key2] === $val2) {
-                    return $savedData[$key];
+        // foreach($savedData as $key => $val) {
+        //     foreach($val as $key2 => $val2) {
+        //         if ($filter[$key2] === $val2) {
+        //             return $savedData[$key];
+        //         }
+        //     }
+        // }
+        $count = 0;
+        $output = array();
+        foreach ($filter as $key => $val) {
+            $count++;
+        }
+        foreach($savedData as $data) {
+            $count2 = 0;
+            foreach($data as $key => $val) {
+                if ($filter[$key] === $val) {
+                    $count2++;
                 }
             }
-        }
-        return $savedData;
-
-    }
-
-    public static function selectAll($table, $filter) {
-        $savedData = file_get_contents(__DIR__."/data/{$table}.json");
-        $savedData = json_decode($savedData);
-        // var_dump($savedData[1]->username);
-        foreach($savedData as $key => $val) {
-            foreach($val as $key2 => $val2) {
-                if ($filter[$key2] === $val2) {
-                    return $savedData[$key];
-                }
+            if ($count2 === $count) {
+                array_push($output, $data);
             }
         }
-        return $savedData;
+        return $output;
 
     }
 }
